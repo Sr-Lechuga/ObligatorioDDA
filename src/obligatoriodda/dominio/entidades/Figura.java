81693f078a2be63c3ed17d6bd38f83bd93879fde
Sr-Lechuga <jonattanlima@gmail.com>
2024-11-10 18:40:39 -0300

    Update: Figura

--- src/obligatoriodda/dominio/entidades/Figura.java
+++ src/obligatoriodda/dominio/entidades/Figura.java
@@ -1,5 +1,17 @@
 package obligatoriodda.dominio.entidades;
 
 public class Figura {
+    private String nombre;
 
+    public Figura(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void analizarMano() {
+        throw new UnsupportedOperationException("Método no implementado");
+    }
 }
